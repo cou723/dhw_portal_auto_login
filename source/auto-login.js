e50935fda4bbde711d81cccedff16e7114f928eb
cou <courange.c@gmail.com>
2022-06-17 11:25:21 +0900

    パスワードが間違えていた場合停止するように修正

--- source/auto-login.js
+++ source/auto-login.js
@@ -1,81 +1,55 @@
+import * as page from './page_decision.js';
+
 console.debug('start');
-console.debug(isPCLoginPage(), isPCAskReLogin(), isAskReLogin());
+console.debug(
+    page.isPCLoginPage(), page.isPCAskReLogin(), page.isSPLoginPage(),
+    page.isSPAskReLogin());
 
 const userName = 'A22DC030';
 const password = '*****';
 
-// PC自動ログイン
-if (isPCLoginPage()) {
-	safety();
-	console.debug('PCLogin');
-	document.querySelector('#loginForm\\:userId').value = userName;
-	document.querySelector('#loginForm\\:password').value = password;
-	document.querySelector('#loginForm\\:loginButton').click();
+if (isPasswordMissing()) {
+  console.debug('password missing');
+  return;
 }
+login_process();
 
-// PC再ログイン時の自動button押下
-if (isPCAskReLogin()) {
-	safety();
-	console.debug('PCReLogin');
-	document.querySelectorAll('.ui-button')[0].click();
+function login_process() {
+  // PC自動ログイン
+  if (page.isPCLoginPage()) {
+    console.debug('PCLogin');
+    document.querySelector('#loginForm\\:userId').value = userName;
+    document.querySelector('#loginForm\\:password').value = password;
+    document.querySelector('#loginForm\\:loginButton').click();
+  }
+
+  // PC再ログイン時の自動button押下
+  if (page.isPCAskReLogin()) {
+    console.debug('PCReLogin');
+    document.querySelectorAll('.ui-button')[0].click();
+  }
+
+  // スマホ自動ログイン
+  if (page.isSPLoginPage()) {
+    console.log('SPLogin');
+    document.querySelector('#pmPage\\:loginForm\\:userId_input').value =
+        userName;
+    // Id="pmPage\\:loginForm\\:password"が二つあるのでnameから取得
+    document.querySelector('[name="pmPage\\:loginForm\\:password"]').value =
+        password;
+    document.querySelector('#pmPage\\:loginForm\\:j_idt38').click();
+  }
+
+  // スマホ再ログイン時の自動button押下
+
+  if (page.isSPAskReLogin()) {
+    // ボタンにidが割り振られていないのでtypeから取得
+    document.querySelector('[type="submit"]').click();
+  }
 }
-
-// スマホ自動ログイン
-if (isSPLoginPage()) {
-	safety();
-	console.log('SPLogin');
-	document.querySelector('#pmPage\\:loginForm\\:userId_input').value = userName;
-	// Id="pmPage\\:loginForm\\:password"が二つあるのでnameから取得
-	document.querySelector('[name="pmPage\\:loginForm\\:password"]').value
-      = password;
-	document.querySelector('#pmPage\\:loginForm\\:j_idt38').click();
-}
-
-// スマホ再ログイン時の自動button押下
-
-if (isSPAskReLogin()) {
-	safety();
-	// ボタンにidが割り振られていないのでtypeから取得
-	document.querySelector('[type="submit"]').click();
-}
-
-// ページ判定関数
-function isPCLoginPage() {
-	return document.querySelector('#loginForm\\:userId')
-      && document.querySelector('#loginForm\\:password')
-      && document.querySelector('#loginForm\\:loginButton');
-}
-
-function isPCAskReLogin() {
-	return isAskReLogin() && document.querySelectorAll('.ui-button')[0];
-}
-
-function isSPLoginPage() {
-	const userId = document.querySelector('#pmPage\\:loginForm\\:userId_input');
-	// Id="pmPage\\:loginForm\\:password"が二つあるので保留
-	// var password = document.getElementById('pmPage\\:loginForm\\:password');
-	const loginButton = document.querySelector('#pmPage\\:loginForm\\:j_idt38');
-	return userId /* && password */ && loginButton;
-}
-
-function isSPAskReLogin() {
-	return isAskReLogin() && document.querySelector('[type="submit"]');
-}
-
-function isAskReLogin() {
-	if (document.querySelectorAll('.innerInfo').length > 0) {
-		return false;
-	}
-
-	return document.querySelectorAll('.innerInfo')[0].textContent
-      === '長時間操作が行われなかったため、自動的にログアウトされました。\nログイン画面より再度ログイン認証を行ってください。';
-}
-
-function safety() {
-	const count = localStorage.getItem('dsc');
-	if (count < 0) {
-		throw new Error('stopper count overflow');
-	} else {
-		localStorage.setItem('dsc', count - 1);
-	}
+function isPasswordMissing() {
+  if (!document.getElementsByClassName('ui-messages-error')[0]) return false;
+  const error = document.getElementsByClassName('ui-messages-error')[0];
+  return 'textContent' in error &&
+      error.textContent === 'ユーザＩＤまたはパスワードが正しくありません。';
 }
